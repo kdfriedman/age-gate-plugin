994c66121223b0fc975dfc84144d7581362b9d44
Kevin Friedman <kev.d.friedman@gmail.com>
2019-09-07 14:49:02 -0400

    tooling setup

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+  
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/app.component.js",
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "public/"),
+    filename: "bundle.js"
+  },
+  devServer:{
+    contentBase: 'public'
+},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
